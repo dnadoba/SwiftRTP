5b1e1a9300054f99c4c2221c617a2c1ca81120cf
David Nadoba <dnadoba@gmail.com>
2020-11-05 21:48:02 +0100

    use GitHub dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "../BinaryKit"),
+        .package(
+            url: "https://github.com/dnadoba/BinaryKit.git",
+            from: "4.0.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
